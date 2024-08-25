5bcf7d43ff8765f96197e1dbf9df4a5af4317918
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-08-25 10:57:30 +0530

    thinkstodo

---
+++ glaizit/src/components/ThingsToDoOnGlaizit.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+
+const ThingsToDoOnGlaizit = () => {
+  return (
+    <div className="bg-white py-8 px-4 sm:px-6 lg:px-8 flex justify-center items-center">
+      <div className="">
+        <div className="relative flex flex-col items-center lg:flex-row lg:justify-between lg:items-start">
+          <div>
+            <img
+              src="/thingstodo.png"
+              alt="Glaizit App"
+              className="w-[781px] h-[722px]"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/thingstodoimages/icon1.png"
+              alt=""
+              className="absolute w-[50px] h-[50px] left-[195px] top-[195px]"
+            />
+            <img
+              src="/thingstodoimages/icon2.png"
+              alt=""
+              className="absolute w-[50px] h-[50px] left-[133px] top-[310px]"
+            />
+            <img
+              src="/thingstodoimages/icon3.png"
+              alt=""
+              className="absolute w-[50px] h-[50px] left-[140px] top-[450px]"
+            />
+            <img
+              src="/thingstodoimages/icon4.png"
+              alt=""
+              className="absolute w-[50px] h-[50px] left-[223px] top-[573px]"
+            />
+            <img
+              src="/thingstodoimages/icon9.png"
+              alt=""
+              className="absolute w-[50px] h-[50px] right-[185px] top-[195px]"
+            />
+            <img
+              src="/thingstodoimages/icon8.png"
+              alt=""
+              className="absolute w-[50px] h-[50px] right-[115px] top-[315px]"
+            />
+            <img
+              src="/thingstodoimages/icon7.png"
+              
+              alt=""
+              className="absolute w-[50px] h-[50px] right-[120px] top-[453px]"
+            />
+            <img
+              src="/thingstodoimages/icon6.png"
+              alt=""
+              className="absolute w-[50px] h-[50px] right-[185px] top-[567px]"
+            />
+          </div>
+
+          {/* <div className="absolute lg:relative w-full lg:w-2/3 flex flex-wrap justify-between items-center lg:pl-10 lg:mt-0 mt-10 space-y-10 lg:space-y-0">
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:top-0">
+              <img
+                src="/thingstodoimages/img1.png"
+                alt="Inspire Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:right-0 lg:top-1/4">
+              <img
+                src="/thingstodoimages/img2.png"
+                alt="Discover Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:right-0 lg:top-1/2">
+              <img
+                src="/thingstodoimages/img3.png"
+                alt="Learn Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:right-0 lg:top-3/4">
+              <img
+                src="/thingstodoimages/img4.png"
+                alt="Discuss Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:bottom-0">
+              <img
+                src="/thingstodoimages/img5.png"
+                alt="Create Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:left-0 lg:top-3/4">
+              <img
+                src="/thingstodoimages/img6.png"
+                alt="Record Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:left-0 lg:top-1/2">
+              <img
+                src="/thingstodoimages/img7.png"
+                alt="Connect Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:left-0 lg:top-1/4">
+              <img
+                src="/thingstodoimages/img8.png"
+                alt="Share Icon"
+                className="w-12 h-12"
+              />
+            </div>
+
+            <div className="flex flex-col items-center space-y-3 lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:top-0">
+              <img
+                src="/thingstodoimages/img9.png"
+                alt="Achieve Icon"
+                className="w-12 h-12"
+              />
+            </div>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThingsToDoOnGlaizit;
--- glaizit/src/views/HomePage.jsx
+++ glaizit/src/views/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "@/components/HeroSection";
 import AboutGlaizitHome from "@/components/AboutGlaizitHome";
+import ThingsToDoOnGlaizit from "@/components/ThingsToDoOnGlaizit";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <AboutGlaizitHome />
+      <ThingsToDoOnGlaizit />
     </>
   );
 };
